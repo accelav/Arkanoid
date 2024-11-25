8b8fdee28870a93be2c2f2565551f48e1b731482
accelav <accelaudiovisual@gmail.com>
2024-11-25 14:43:26 +0100

    Creé la base del juego Arkanoid
    Creé los objetos básicos y los scripts de movimiento básico

---
+++ Assets/Scripts/Gameplay.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gameplay : MonoBehaviour
+{
+    [SerializeField]
+    GameObject esfera;
+    [SerializeField]
+    GameObject plataforma;
+    [SerializeField]
+    GameObject botonInicar;
+    [SerializeField]
+    GameObject botónOpciones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
